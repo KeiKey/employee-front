f13aadc4b0fb89ebee8a0a999713897c90c98225
keiselami <kei@aledia.ca>
2021-02-05 23:47:11 +0100

    Employee service and model

---
+++ src/app/employee.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Employee} from './employee';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class EmployeeService {
+
+    private apiServerUrl = 'http://localhost:8080/api/v1/employee/';
+
+    constructor(private http: HttpClient) {    }
+
+    public getEmployee(employeeId: Number): Observable<Employee> {
+        return this.http.get<Employee>(`${this.apiServerUrl}/${employeeId}`);
+    }
+
+    public getEmployees(): Observable<Employee[]> {
+        return this.http.get<Employee[]>(`${this.apiServerUrl}/all`);
+    }
+
+    public addEmployee(employee: Employee): Observable<Employee> {
+        return this.http.post<Employee>(`${this.apiServerUrl}/create`, employee);
+    }
+
+    public updateEmployee(employee: Employee): Observable<Employee> {
+        return this.http.put<Employee>(`${this.apiServerUrl}/update`, employee);
+    }
+
+    public deleteEmployee(employeeId: Number): Observable<void> {
+        return this.http.delete<void>(`${this.apiServerUrl}/delete/${employeeId}`);
+    }
+
+}
---
+++ src/app/employee.ts
@@ -0,0 +1,9 @@
+export interface Employee {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+  pfp: string;
+  phone: string;
+  jobTitle: string;
+}
